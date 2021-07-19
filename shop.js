828971dd8b3ec7851fd2b6a570b820a87aa244f0
Danny <danny20012011@icloud.com>
2021-07-19 15:48:05 -0400

    Fixed the shop page removed home icon

--- shop.js
+++ shop.js
@@ -1,5 +1,10 @@
 const triggerFlip = document.querySelector('.Card')
 const box2 = document.querySelector('#box2')
+const box3 = document.querySelector('#box3')
+const box4 = document.querySelector('#box4')
+const box5 = document.querySelector('#box5')
+const box6 = document.querySelector('#box6')
+
 
 
 triggerFlip.addEventListener('click', function() {
@@ -10,4 +15,19 @@ box2.addEventListener('click', function() {
   box2.classList.toggle('flipped')
 })
 
+box3.addEventListener('click', function() {
+    box3.classList.toggle('flipped')
+  })
+
+  box4.addEventListener('click', function() {
+    box4.classList.toggle('flipped')
+  })
+
+  box5.addEventListener('click', function() {
+    box5.classList.toggle('flipped')
+  })
+
+  box6.addEventListener('click', function() {
+    box6.classList.toggle('flipped')
+  })
 
